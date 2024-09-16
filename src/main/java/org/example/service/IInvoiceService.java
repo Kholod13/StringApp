efafa06696cc688ecdc45e4d5a3fe0a4fe6268cc
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-09-16 21:28:06 +0200

    update

---
+++ src/main/java/org/example/service/IInvoiceService.java
@@ -0,0 +1,27 @@
+package org.example.service;
+
+import org.example.model.Invoice;
+
+import java.util.List;
+
+public class IInvoiceService {
+    public Invoice saveInvice(Invoice invoice) {
+        return null;
+    }
+
+    public List<Invoice> getAllInvoices() {
+        return null;
+    }
+
+    public Invoice getInvoiceById(Long id) {
+        return null;
+    }
+
+    public void deleteInvoiceById(Long id) {
+
+    }
+
+    public void updateInvoice(Invoice invoice) {
+
+    }
+}
